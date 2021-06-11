39cc8e1be0b0664d562e7f022934c8ddfdb84e9d
retromodernism <43184201+retromodernism@users.noreply.github.com>
2021-06-11 21:02:42 +0800

    Добавил Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,37 @@
 import "./index.scss";
-
+import Li from "./li";
+import logo from "./src/logo.png";
 const Header = (props) => {
-  return <header className="header container"></header>;
+  return (
+    <header className="header container">
+      <div className="header__content">
+        <a href="#" className="header__logo">
+          <picture className="header__logo-picture">
+            <source srcSet={logo} media="(min-width: 0)" />
+            <img src="#" alt="" className="header__logo-img" />
+          </picture>
+        </a>
+        <h1 className="header__title DisplayLarge">
+          YOUR <span className="header__title-marked">PERSONALIZED</span> COFFEE
+        </h1>
+        <div className="header__img"></div>
+      </div>
+      <nav className="header__nav">
+        <div className="header__nav-cart">
+          <div className="header__nav-cart-icon"></div>
+          <div className="header__nav-cart-count">2</div>
+        </div>
+        <ul className="header__nav-ul">
+          <Li title="TRANG CHỦ" />
+          <Li title="COFFEE" />
+          <Li title="PHIN MẠ MÀU" />
+          <Li title="COMBO PHIN PHÊ" />
+          <Li title="GIFTSET" />
+          <Li title="LIÊN HỆ" />
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/li/index.jsx
@@ -0,0 +1,26 @@
+import "./index.scss";
+import { useState } from "react";
+
+const Li = ({ title, href = "#" }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const setHover = () => setIsHovered(true);
+  const unsetHover = () => setIsHovered(false);
+
+  return (
+    <li className="header__nav-ul-li">
+      <a
+        href={href}
+        className={
+          isHovered ? "header__nav-link Button" : "header__nav-link Body"
+        }
+        onMouseEnter={setHover}
+        onMouseLeave={unsetHover}
+      >
+        {title}
+      </a>
+    </li>
+  );
+};
+
+export default Li;
